15b06854cab890b0400f42dfb1327cbcc2ea1bde
Juliandross <111920433+juliandross@users.noreply.github.com>
2024-10-23 11:37:52 -0500

    Ultimo fix

--- spline3.h
+++ spline3.h
@@ -1,6 +1,4 @@
 #ifndef SPLINE3_H
-#define SPLINE3_H
-
 #include <cmath>
 #include <vector>
 #include <iostream>
@@ -13,9 +11,6 @@ using std::endl;
 namespace interpolacion {
 	class spline3 {
 	public:
-		/**
-		* @brief crea una nueva instancia de interpolador por splines cubicos
-		*/
 		spline3(vector<double> x, vector<double> y) : x(x), y(y) {
 			construir();
 		}
@@ -24,35 +19,35 @@ namespace interpolacion {
 			return valido;
 		}
 		
-		/**
-		* @brief Evalua el spline del intervalo en el que se encuentra x_int
-		* @param x_int valor a interpolar
-		* @return valor de spline en x_int
-		*/
 		double interpolar(double x_int) {
 			int i;
 			for (i = 0; i < (int)x.size() && x_int > x[i]; i++);
+			
+			
+			// Imprimir los límites generales del intervalo
+			cout << "Intervalo general: [" << x.front() << ", " << x.back() << "]" << endl;
+			
 			cout << "Intervalo en el que se encuentra el dato " << x_int << " : " << i << endl;
 			
 			// Usando los nodos intermedios
 			if (i == 0 || i == (int)x.size()) {
 				return NAN;
 			}
+			
+			// Imprimir el polinomio para el intervalo específico
+			imprimir_polinomios();
+			
 			return evaluar(i - 1, x_int);
 		}
 		
-		/**
-		* @brief Imprimir el error relativo y el error relativo porcentual
-		* @param valor_real valor real del dato
-		* @param valor_interpolado valor interpolado obtenido
-		*/
 		void imprimir_error(double valor_real, double valor_interpolado) {
 			if (valor_real == 0) {
 				cout << "El valor real es 0, el error relativo no se puede calcular." << endl;
 				return;
 			}
 			
-			double error_relativo = fabs((valor_real - valor_interpolado) / valor_real);
+			// Calculamos el error absoluto y relativo correctamente
+			double error_relativo = fabs((valor_real - valor_interpolado) / fabs(valor_real));
 			double error_relativo_porcentual = error_relativo * 100;
 			
 			cout << "Error Relativo: " << error_relativo << endl;
@@ -72,7 +67,6 @@ namespace interpolacion {
 					cout << "Interpolación inválida" << endl;
 					return;
 				}
-				//cosas que no entendí(alpha) pero funciona
 				vector<double> h(n - 1), alpha(n - 1);
 				for (int i = 0; i < n - 1; i++) {
 					h[i] = x[i + 1] - x[i];
@@ -103,12 +97,6 @@ namespace interpolacion {
 				valido = true;
 			}
 			
-			/**
-			* @brief Evaluar el spline del intervalo i para x_int
-			* @param i intervalo
-			* @param x_int valor a interpolar
-			* @return valor interpolado
-			*/
 			double evaluar(int i, double x_int) {
 				if (!valido) {
 					return NAN;
@@ -121,7 +109,24 @@ namespace interpolacion {
 				double dx = x_int - x[i];
 				return A * dx * dx * dx + B * dx * dx + C * dx + D;
 			}
+			
+			void imprimir_polinomios() {
+				for (int i = 0; i < (int)x.size() - 1; i++) {
+					double h = x[i + 1] - x[i];
+					double A = (f2[i + 1] - f2[i]) / (6.0 * h);
+					double B = f2[i] / 2.0;
+					double C = (y[i + 1] - y[i]) / h - (f2[i + 1] + 2.0 * f2[i]) * h / 6.0;
+					double D = y[i];
+					
+					cout << "Polinomio correspondiente en el intervalo [" << x[i] << ", " << x[i + 1] << "]:\n";
+					cout << "S"<<(i+1)<<"(x) = " << A << "(x - " << x[i] << ")^3 + " 
+						<< B << "(x - " << x[i] << ")^2 + " 
+						<< C << "(x - " << x[i] << ") + " 
+						<< D << endl;
+				}
+			}
 	};
 }
+
 
 #endif
